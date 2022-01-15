b5904a2447ef99f93e59e5e13cc3f2f114d26216
Samid77 <dimasseptyantop@gmail.com>
2022-01-15 16:36:36 +0700

    Finish server functionality

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,23 @@
 const postsResolvers = require('./posts.js')
 const usersResolvers = require('./users.js')
+const commentsResolvers = require('./comments.js')
 
 module.exports = {
+    Post: {
+      likeCount(parent) {
+        return parent.likes.length;
+      },
+      commentCount: (parent) => parent.comments.length
+    },
     Query: {
       ...postsResolvers.Query
     },
     Mutation: {
-      ...usersResolvers.Mutation
+      ...usersResolvers.Mutation,
+      ...postsResolvers.Mutation,
+      ...commentsResolvers.Mutation,
+    },
+    Subscription: {
+      ...postsResolvers.Subscription
     }
 }
