6dbc2277ab14a3cb3e75cef4865d6f2e5c210e7b
Samid77 <dimasseptyantop@gmail.com>
2022-01-18 14:51:06 +0700

    Deleting post from list or from detail psot

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -19,7 +19,11 @@ function PostCard({
         <Image
           floated="right"
           size="mini"
-          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+          src={
+            username === 'lionel.messi' ? "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR_BSXPlBjoBeJruSaCamv7kQuMNjoIIWX0CITXUVoapFCbRM9g"
+            : username === 'mesut.ozil' ? "https://upload.wikimedia.org/wikipedia/commons/1/12/Mesut_%C3%96zil_at_Baku_before_2019_UEFA_Europe_League_Final.jpg"
+            : username === 'samid' ? "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Brad_Pitt_2019_by_Glenn_Francis.jpg/1200px-Brad_Pitt_2019_by_Glenn_Francis.jpg"
+            : "https://www.w3schools.com/w3images/avatar2.png" }
         />
         <Card.Header>{username}</Card.Header>
         <Card.Meta as={Link} to={`/posts/${id}`}>
